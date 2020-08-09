d77dba5f2ffdb07ba20e824da3e67c7a5ea45bc2
Mazhit <mig7676@yandex.ru>
2020-08-09 17:42:53 +0300

    Добавил решение по задачи №3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -12,6 +12,7 @@
 from timeit import timeit
 import cProfile
 
+
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
         return enter_num,revers_num
@@ -19,7 +20,7 @@ def revers(enter_num, revers_num=0):
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        revers(enter_num, revers_num)
+        revers(enter_num, revers_num) # O(2^n)+сильно ограничение по стеку
 
 
 def revers_2(enter_num, revers_num=0):
@@ -27,15 +28,15 @@ def revers_2(enter_num, revers_num=0):
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-    return revers_num
+    return revers_num               # O(2^n)+сильно ограничение по стеку
 
 
-def revers_3(enter_num):
+def revers_3(enter_num):# Самая эффективная функция исходя из расчета времени
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
-    return revers_num
+    return revers_num               #Срез, списка O(n)
 
-def main(a):
+def my_f(a):
     print(revers(a))
     print(revers_2(a))
     print(revers_3(a))
@@ -44,6 +45,13 @@ def main(a):
 
 
 a=int(input("Input count:"))
-main(a)
-cProfile.run('main(a),a')
+c=list(range(a))
+a=int(''.join(map(str, c)))
+print(a)
 
+print(timeit("revers(a)", setup="from __main__ import revers, a", number=1000))
+print(timeit("revers_2(a)", setup="from __main__ import revers_2, a", number=1000))
+print(timeit("revers_3(a)", setup="from __main__ import revers_3, a", number=1000))
+
+
+cProfile.run('my_f(a),a')
